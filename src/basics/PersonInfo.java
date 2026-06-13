98fcfb2fd78f0915047bcd01229465d7f4896cb8
netjs <infojtutor@gmail.com>
2026-06-13 18:03:19 +0530

    Code for exercise

---
+++ src/basics/PersonInfo.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class PersonInfo {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------------------------------");
+		System.out.println("Name \t Age \t DOB");
+		System.out.println("-----------------------------------------");
+		System.out.println("Ram \t 32 \t 07/07/1997");
+	}
+
+}
